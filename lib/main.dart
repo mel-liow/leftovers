9396415d1c81c07c8320f8e75c5050575ee514ce
mel-liow <mel.liow.94@gmail.com>
2020-12-06 10:28:39 +0000

    feat: add favourite  context

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:leftovers_app/models/favouriteModel.dart';
 import 'package:provider/provider.dart';
 import 'package:leftovers_app/models/cartModel.dart';
 import 'package:leftovers_app/screens/home/home.dart';
@@ -22,9 +23,16 @@ class MyApp extends StatelessWidget {
           Provider(create: (context) => FoodCatalogue()),
           ChangeNotifierProxyProvider<FoodCatalogue, CartModel>(
             create: (context) => CartModel(),
-            update: (context, catalog, cart) {
-              cart.catalog = catalog;
+            update: (context, items, cart) {
+              cart.cartItems = items;
               return cart;
+            },
+          ),
+          ChangeNotifierProxyProvider<FoodCatalogue, FavouriteModel>(
+            create: (context) => FavouriteModel(),
+            update: (context, items, favourite) {
+              favourite.favouriteItems = items;
+              return favourite;
             },
           ),
         ],
